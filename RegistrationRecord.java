f637cc71b55b56566963a354b70ef4be8c6d440d
jennase <54061009+jennase@users.noreply.github.com>
2019-08-13 16:26:51 +0700

    Add files via upload

---
+++ RegistrationRecord.java
@@ -0,0 +1,34 @@
+package jenna;
+
+public class RegistrationRecord {
+	private Student student;
+	private Subject [] subjects;
+	int count = 0;
+	
+	public RegistrationRecord() {
+		student = new Student();
+		subjects = new Subject[10];
+		for(int i=0; i < 10; i++){
+			subjects[i] = new Subject();
+		}
+	}
+	public RegistrationRecord(int n) {
+		count = 0;
+		student = new Student();
+		subjects = new Subject[n];
+		for(int i=0; i < n; i++){
+			subjects[i] = new Subject();
+		}
+	}
+	public void addSubject(Subject s) {
+		if (count < subjects.length -1) {
+			subjects[count] = s;
+			count++;
+		}
+	}
+	public void display(){
+		for(int i=0; i < count; i++){
+		System.out.println(subjects[i]);
+	}
+	}
+}
